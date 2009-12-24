dce73c99f82cf55bbdf61bd6ec74aa1619695bfc
tkoomzaaskz <tkoomzaaskz@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2009-12-24 13:01:51 -0600

    tdVisitorCounter plugin ready for 0.1.1 release
    git-svn-id: http://svn.symfony-project.com/plugins/tdVisitorCounterPlugin/trunk@25890 ee427ae8-e902-0410-961c-c3ed070cd9f9

---
+++ lib/tdVisitorManager.class.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * tdVisitorManager.class
+ */
+
+/**
+ * tdVisitorManager
+ *
+ * @author Tomasz Ducin <tomasz.ducin@gmail.com>
+ * @package symfony
+ * @subpackage tdVisitorCounterPlugin
+ */
+
+class tdVisitorManager
+{
+  /**
+   * Mark a visitor: check if the visitor's browser already has a cookie set,
+   * if not - insert a database record.
+   */
+  public function markVisitor()
+  {
+    if (!$_COOKIE['counter'])
+    {
+      setcookie('counter', 'visited', time() + sfConfig::get('td_visitor_counter_cookie_interval'));
+      tdCounterTable::mark();
+    }
+  }
+}
